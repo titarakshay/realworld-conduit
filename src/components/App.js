341416165c08ae04a466c7b82dec1a619120053c
titarakshay <titarakshay@gmail.com>
2020-10-08 23:42:52 +0530

    home-login-signup pages created

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "react-bootstrap";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import Homepage from "./Homepage";
 import LoginPage from "./LoginPage";
 import SignupPage from "./SignupPage";
+import Header from "./Header";
+import UserPage from "./UserPage";
 
 function APP() {
+  const [isLogged, setisLogged] = useState(false);
+  useEffect(() => {
+    if (localStorage.authToken) {
+      setisLogged(true);
+    }
+  }, []);
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/signup" component={SignupPage} />
-      </Switch>
+      <div className="body-wrapper">
+        <Header isLogged={isLogged} setisLogged={setisLogged} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route
+            exact
+            path="/login"
+            render={() => <LoginPage setisLogged={setisLogged} />}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={() => <SignupPage setisLogged={setisLogged} />}
+          />
+          <Route path="/user" component={UserPage} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,8 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 
-function SignupPage() {
-  return <h1>SignupPage</h1>;
+function SignupPage(props) {
+  const [email, setemail] = useState("");
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [terms, setterms] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const url = "https://mighty-oasis-08080.herokuapp.com/api/users";
+    console.log(username, email, password, "here are");
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          email,
+          password,
+          username,
+        },
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then(({ user }) => {
+        user && localStorage.setItem("authToken", user.token);
+        props.setisLogged(true);
+        props.history.push("/");
+      })
+      .catch((error) => console.log(error, "error is here"));
+  }
+
+  let disable = !email || !username || !password || !terms;
+  return (
+    <div className="margin-top form-container">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div class="form-group">
+          <label>Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            name="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div class="form-group">
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setusername(e.target.value)}
+            class="form-control"
+          />
+        </div>
+        <div class="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+            class="form-control"
+          />
+        </div>
+
+        <div class="form-group form-check">
+          <input
+            type="checkbox"
+            name="terms"
+            value={terms}
+            onChange={() => setterms(!terms)}
+            class="form-check-input"
+          />
+          <label class="form-check-label">
+            I accept the terms and condtion
+          </label>
+        </div>
+        <button type="submit" disabled={disable} class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default withRouter(SignupPage);
